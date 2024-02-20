747b1e7199d74d56dc12587f263f637590d0951a
tw990106 <118413280+tw990106@users.noreply.github.com>
2024-02-21 04:59:41 +0900

    .

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ let news = [];
 
 // http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines
 
+
 const getLatestNews = async() => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = new URL(`https://playful-bienenstitch-9af164.netlify.app/top-headlines?`);
     const response = await fetch(url);
     const data = response.json(); // json은 파일 형식 중 하나.
     news = data.articles;

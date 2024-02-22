f09b6c175e08cf5f3e9b5dfc76f00c19588af63a
tw990106 <118413280+tw990106@users.noreply.github.com>
2024-02-23 05:44:35 +0900

    .

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const API_KEY = `fa3bf53b47ed4acebebefefbfc4c03e1`;
 let newsList = [];
+const menus = document.querySelectorAll('.menus button');
+menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategory(event)))
 
 // http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines
 
@@ -13,6 +15,31 @@ const getLatestNews = async() => {
     console.log('ddd', newsList);
 };
 
+const getNewsByCategory = async (event) => {
+    const category = event.target.textContent.toLowerCase();
+    console.log('category', category);
+    const url = new URL(`https://playful-bienenstitch-9af164.netlify.app/top-headlines?category=${category}`);
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log('ddd', data);
+
+    newsList = data.articles;
+    render();
+}
+
+const getNewsByKeyword = async () => {
+    const keyword = document.getElementById('search-input').value;
+    console.log("keyword", keyword);
+
+    const url = new URL(`https://playful-bienenstitch-9af164.netlify.app/top-headlines?q=${keyword}`);
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log('keyword data', data);
+
+    newsList = data.articles;
+    render();
+}
 
 
 // side menu open
@@ -44,7 +71,7 @@ const render = () => {
 
     `<div class="row news">
                 <div class="col-lg-4">
-                    <img class="news-img-size" src=${news.urlToImage || 'https://st3.depositphotos.com/23594922/31822/v/450/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg'}>
+                    <img class="news-img-size" src=${news.urlToImage || "https://st3.depositphotos.com/23594922/31822/v/450/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"}>
                 </div>
                 <div class="col-lg-8 news-txt">
                     <h2>${news.title}</h2>
@@ -55,7 +82,7 @@ const render = () => {
                         ? news.description.substring(0, 200) + '...'
                         : news.description
                     }</p>
-                    <div>${news.source.name || 'no source'} * ${moment(news.publishedAt).startOf('day').fromNow()}</div>
+                    <div>${news.source.name || 'no'} * ${moment(news.publishedAt).startOf('day').fromNow()}</div>
                 </div>
             </div>`).join('');
     
@@ -68,3 +95,4 @@ const render = () => {
 
 
 getLatestNews();
+

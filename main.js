c555c5a921beccccf65f5ce7cfb5d8529fa9d248
tw990106 <118413280+tw990106@users.noreply.github.com>
2024-02-23 23:07:03 +0900

    .

--- main.js
+++ main.js
@@ -77,7 +77,13 @@ const toggleSearch = () => {
 }
 
 
+// img error
+const imgError = (image) => {
+    image.onerror = null;
+    image.src = "https://st3.depositphotos.com/23594922/31822/v/450/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg";
+};
 
+// news contents
 const render = () => {
 
     const newsHTML = newsList.map((news) => 
@@ -85,7 +91,7 @@ const render = () => {
 
     `<div class="row news">
                 <div class="col-lg-4">
-                    <img class="news-img-size" src=${news.urlToImage || "https://st3.depositphotos.com/23594922/31822/v/450/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"}>
+                    <img class="news-img-size" src=${news.urlToImage} onerror = "imgError(this)">
                 </div>
                 <div class="col-lg-8 news-txt">
                     <h2>${news.title}</h2>
